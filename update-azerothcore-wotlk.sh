b13e9b7050bdd18334e44e55824b296cf0f638b8
hat3ph <88069788+hat3ph@users.noreply.github.com>
2026-06-29 14:47:54 +0800

    beautify the code

--- update-azerothcore-wotlk.sh
+++ update-azerothcore-wotlk.sh
@@ -16,35 +16,42 @@ function update(){
                 echo "###############################"
                 sudo systemctl stop ac-authserver
                 sudo systemctl stop ac-worldserver
+                echo "Stoping Azerothcore services."
 
+                echo -e "\n"
                 echo "#####################################"
                 echo "Updating Azerothcore git directory..."
                 echo "#####################################"
-                cd $AC_CODE_DIR
+                cd ${AC_CODE_DIR}
                 git pull
 
+                echo -e "\n"
                 echo "#################################"
                 echo "Checking latesting client data..."
                 echo "#################################"
-                LATEST_CLIENT=$(curl -s https://api.github.com/repos/wowgaming/client-data/releases/latest 2>/dev/null | grep '"tag_name"' | cut -d'"' -f4 || echo "unknown")
-                CURRENT_CLIENT=$(cat $AC_CODE_DIR/data/.version 2>/dev/null || echo "unknown")
-                if [ "$CURRENT_CLIENT" = "$LATEST_CLIENT" ] && [ "$CURRENT_CLIENT" != "unknown" ]; then
+                LATEST_CLIENT=$(curl -s https://api.github.com/repos/wowgaming/client-data/releases/latest 2>/dev/null | \
+                        grep '"tag_name"' | cut -d'"' -f4 || echo "unknown")
+                CURRENT_CLIENT=$(cat ${AC_CODE_DIR}/data/.version 2>/dev/null || echo "unknown")
+                if [ "${CURRENT_CLIENT}" = "${LATEST_CLIENT}" ] && [ "${CURRENT_CLIENT}" != "unknown" ]; then
                         echo "Client data is up to date"
                 else
                         echo "############################################"
                         echo "Updating to latest version of client data..."
                         echo "############################################"
                         echo -e "\n"
-                        wget -q --show-progress https://github.com/wowgaming/client-data/releases/download/${LATEST_CLIENT}/Data.zip -P /tmp
-                        unzip /tmp/Data.zip -d $AC_CODE_DIR/data
-                        echo "$LATEST_CLIENT" > $AC_CODE_DIR/data/.version
+                        wget -q --show-progress https://github.com/wowgaming/client-data/releases/download/${LATEST_CLIENT}/Data.zip \
+                                -P /tmp
+                        unzip /tmp/Data.zip -d ${AC_CODE_DIR}/data
+                        echo "${LATEST_CLIENT}" > ${AC_CODE_DIR}/data/.version
+                        echo "Done update client data to version ${LATEST_CLIENT}."
                 fi
 
+                echo -e "\n"
                 echo "###########################"
                 echo "Re-compiling Azerothcore..."
                 echo "###########################"
-                mkdir -p $AC_CODE_DIR/build && cd $AC_CODE_DIR/build
-                cmake ../ -DCMAKE_INSTALL_PREFIX=$AC_CODE_DIR/env/dist/ \
+                mkdir -p ${AC_CODE_DIR}/build && cd ${AC_CODE_DIR}/build
+                cmake ../ -DCMAKE_INSTALL_PREFIX=${AC_CODE_DIR}/env/dist/ \
                         -DCMAKE_C_COMPILER=/usr/bin/clang \
                         -DCMAKE_CXX_COMPILER=/usr/bin/clang++ \
                         -DWITH_WARNINGS=1 \
@@ -54,36 +61,42 @@ function update(){
                 #export BUILD_CORES=`nproc | awk '{print $1 - 1}'`
                 make -j$(nproc)
                 make install
+                echo "Done compling latest Azerothcore."
 
                 # copy and create Azerothcore config files
+                echo -e "\n"
                 echo "#####################################"
                 echo "Configure Azerothcore config files..."
                 echo "#####################################"
-                cp $AC_CODE_DIR/env/dist/etc/authserver.conf.dist $AC_CODE_DIR/env/dist/etc/authserver.conf
-                cp $AC_CODE_DIR/env/dist/etc/worldserver.conf.dist $AC_CODE_DIR/env/dist/etc/worldserver.conf
+                cp ${AC_CODE_DIR}/env/dist/etc/authserver.conf.dist ${AC_CODE_DIR}/env/dist/etc/authserver.conf
+                cp ${AC_CODE_DIR}/env/dist/etc/worldserver.conf.dist ${AC_CODE_DIR}/env/dist/etc/worldserver.conf
 
                 # configure worldserver.conf
                 sed -i "s|^LoginDatabaseInfo.*|LoginDatabaseInfo = \"127.0.0.1;3306;acore;${DB_PASS};acore_auth\"|" \
-                        $AC_CODE_DIR/env/dist/etc/worldserver.conf
+                        ${AC_CODE_DIR}/env/dist/etc/worldserver.conf
                 sed -i "s|^WorldDatabaseInfo.*|WorldDatabaseInfo = \"127.0.0.1;3306;acore;${DB_PASS};acore_world\"|" \
-                        $AC_CODE_DIR/env/dist/etc/worldserver.conf
+                        ${AC_CODE_DIR}/env/dist/etc/worldserver.conf
                 sed -i "s|^CharacterDatabaseInfo.*|CharacterDatabaseInfo = \"127.0.0.1;3306;acore;${DB_PASS};acore_characters\"|" \
-                        $AC_CODE_DIR/env/dist/etc/worldserver.conf
-                sed -i "s|^DataDir.*|DataDir = \"$AC_CODE_DIR/data\"|" $AC_CODE_DIR/env/dist/etc/worldserver.conf
-                sed -i "s|^LogsDir.*|LogsDir = \"$AC_CODE_DIR/logs\"|" $AC_CODE_DIR/env/dist/etc/worldserver.conf
-                sed -i "s|^TempDir.*|TempDir = \"$AC_CODE_DIR/temp\"|" $AC_CODE_DIR/env/dist/etc/worldserver.conf
+                        ${AC_CODE_DIR}/env/dist/etc/worldserver.conf
+                sed -i "s|^DataDir.*|DataDir = \"${AC_CODE_DIR}/data\"|" ${AC_CODE_DIR}/env/dist/etc/worldserver.conf
+                sed -i "s|^LogsDir.*|LogsDir = \"${AC_CODE_DIR}/logs\"|" ${AC_CODE_DIR}/env/dist/etc/worldserver.conf
+                sed -i "s|^TempDir.*|TempDir = \"${AC_CODE_DIR}/temp\"|" ${AC_CODE_DIR}/env/dist/etc/worldserver.conf
+                echo "Done configure ${AC_CODE_DIR}/env/dist/etc/worldserver.conf."
 
                 # configure authserver.conf
                 sed -i "s|^LoginDatabaseInfo.*|LoginDatabaseInfo = \"127.0.0.1;3306;acore;${DB_PASS};acore_auth\"|" \
-                        $AC_CODE_DIR/env/dist/etc/authserver.conf
-                sed -i "s|^LogsDir.*|LogsDir = \"$AC_CODE_DIR/logs\"|" $AC_CODE_DIR/env/dist/etc/authserver.conf
-                sed -i "s|^TempDir.*|TempDir = \"$AC_CODE_DIR/temp\"|" $AC_CODE_DIR/env/dist/etc/authserver.conf
+                        ${AC_CODE_DIR}/env/dist/etc/authserver.conf
+                sed -i "s|^LogsDir.*|LogsDir = \"${AC_CODE_DIR}/logs\"|" ${AC_CODE_DIR}/env/dist/etc/authserver.conf
+                sed -i "s|^TempDir.*|TempDir = \"${AC_CODE_DIR}/temp\"|" ${AC_CODE_DIR}/env/dist/etc/authserver.conf
+                echo "Done configure ${AC_CODE_DIR}/env/dist/etc/authserver.conf."
 
+                echo -e "\n"
                 echo "###############################"
                 echo "Start Azerothcore services..."
                 echo "###############################"
                 sudo systemctl start ac-authserver
                 sudo systemctl start ac-worldserver
+                echo "Done started Azerothcore service."
         fi
 }
 
